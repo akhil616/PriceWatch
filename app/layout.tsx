f42aebde146e25050b3801bfb250caa511780a9f
Akhil Chauhan <109506738+akhil616@users.noreply.github.com>
2024-04-22 12:13:35 +0530

    Update layout.tsx - font issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,14 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
